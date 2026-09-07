3391a02fa41169d46d2be5138feadaaf531a16d9
Sdoba16 <asdobnov16@gmail.com>
2026-09-07 11:20:00 +0200

    add source observations and their rejection checks

--- price-feed/src/lib.rs
+++ price-feed/src/lib.rs
@@ -2,7 +2,9 @@ pub mod clock;
 pub mod constants;
 pub mod price_data;
 pub mod registry;
+pub mod source;
 
 pub use clock::Clock;
 pub use price_data::{DecodeError, PRICE_FEED_DATA_LEN, PriceFeedData};
 pub use registry::{Asset, FeedDefinition, FeedId, FeedKind, FeedRegistry};
+pub use source::{ConnectionError, PriceSource, RejectionReason, SourceObservation, SourceState};
