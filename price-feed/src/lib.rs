1ea02db780e3fafa15b3cc6413ea59a8e0c96e97
Sdoba16 <asdobnov16@gmail.com>
2026-09-07 11:20:02 +0200

    add the median reducer

--- price-feed/src/lib.rs
+++ price-feed/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod clock;
 pub mod constants;
 pub mod price_data;
+pub mod reduce;
 pub mod registry;
 pub mod source;
 
 pub use clock::Clock;
 pub use price_data::{DecodeError, PRICE_FEED_DATA_LEN, PriceFeedData};
+pub use reduce::{MedianReducer, Reducer};
 pub use registry::{Asset, FeedDefinition, FeedId, FeedKind, FeedRegistry};
 pub use source::{ConnectionError, PriceSource, RejectionReason, SourceObservation, SourceState};
