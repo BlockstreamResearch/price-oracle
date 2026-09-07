6903eeb0aab86160243168f824aea0b345b643b2
Sdoba16 <asdobnov16@gmail.com>
2026-09-07 11:19:56 +0200

    add the constants every node must agree on

---
+++ price-feed/src/constants.rs
@@ -0,0 +1,7 @@
+pub const VALIDITY_WINDOW: u64 = 60;
+pub const SOURCE_DATA_VALIDITY_DURATION: u64 = 120;
+pub const MAX_CLOCK_SKEW: u64 = 5;
+pub const POLLING_INTERVAL: u64 = 30;
+pub const MAX_POLLING_ERROR_NUM: u32 = 5;
+pub const POLLING_RETRY_TIME: u64 = 300;
+pub const BACKOFF_CAP: u64 = 30;
